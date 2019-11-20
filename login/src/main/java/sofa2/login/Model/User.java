36ba28dc5db05ccab9727c39c5a48185ab10b8e3
aatniehot <56220714+aatniehot@users.noreply.github.com>
2019-11-20 11:44:53 +0100

    model, view
    added model + view

--- login/src/main/java/sofa2/login/Model/User.java
+++ login/src/main/java/sofa2/login/Model/User.java
@@ -1,4 +1,43 @@
 package sofa2.login.Model;
 
 public class User {
+
+    private String username;
+    private String password;
+    private Enum userRole;
+
+    public User() {
+        super();
+    }
+
+    public User(String username, String password, Enum userRole) {
+        super();
+        this.username = username;
+        this.password = password;
+        this.userRole = userRole;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Enum getUserRole() {
+        return userRole;
+    }
+
+    public void setUserRole(Enum userRole) {
+        this.userRole = userRole;
+    }
 }
